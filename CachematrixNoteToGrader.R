18a3651bfe036ab6210bd2d565ba7431d9bd4c9f
cameroncramer <cam@miprofs.com>
2014-07-28 12:02:12 -0700

    Please see notes to Grader

--- CachematrixNoteToGrader.R
+++ CachematrixNoteToGrader.R
@@ -1,1 +1,2 @@
-## I mistakenly renamed cachematrix to ProgrammingAssignment2, but the work is all there as per the assignment.
+## Mistakenly renamed cachematrix to ProgrammingAssignment2, but the work was submitted on time as per the assignment -- 
+ProgrammingAssignment2 (8a4c01e090e3aba5ced0c42ba7cbef6cdc144aee)
--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){							#Sets the value of the matrix
+  x<<-y
+  m<<-NULL
+}
+get<-function() x							#Gets the value of the matrix
+setmatrix<-function(solve) m<<- solve		#Sets the value of the inverse matrix
+getmatrix<-function() m						#Gets the value of the inverse matrix	
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()						#Checks if matrix inverse already calculated
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()							#Calculates matrix inverse if not done already 
+    m<-solve(matrix, ...)					#Defines m as the matrix inverse
+    x$setmatrix(m)
+    m										#Returns the matrix inverse
